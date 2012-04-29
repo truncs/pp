8ed065fab03943d1494b78ca3b81f1867cbe5525
truncs <sarawgi.aditya@gmail.com>
2012-04-29 16:49:52 -0400

    changes to pick up arrays from indexes 0

--- hw3/task1/a/prefix_sum.cpp
+++ hw3/task1/a/prefix_sum.cpp
@@ -6,26 +6,26 @@ using namespace std;
 void prefix_sum(vector<int> & x, vector<int>& s) {
 
   int n = x.size();
-  vector<int> y(n/2 + 1);
+  vector<int> y(n/2);
 
-  if (n == 2)
-	s[1] = x[1];
+  if (n == 1)
+	s[0] = x[0];
 
   else {
-	for (int i = 1; i < n/2; ++i)
+	for (int i = 0; i < n/2; ++i)
 	  {
-		y[i] = x[2*i -1] + x[2*i];
+		y[i] = x[2*i] + x[2*i + 1];
 	  }
-	vector<int> z(n/2 + 1);
+	vector<int> z(n/2);
 	prefix_sum(y, z);
-	for (int i = 1; i < n; ++i)
+	for (int i = 0; i < n; ++i)
 	  {
-		if (i == 1)
-		  s[1] = x[1];
-		else if (i % 2 == 0)
-		  s[i] = z[i/2];
+		if (i == 0)
+		  s[0] = x[0];
+		else if (i % 2 != 0)
+		  s[i] = z[(i - 1)/2];
 		else
-		  s[i] = z[(i - 1) / 2] + x[i];
+		  s[i] = z[(i - 2) / 2] + x[i];
 			
 	  }
   }
@@ -35,18 +35,17 @@ void prefix_sum(vector<int> & x, vector<int>& s) {
 
 int main() {
   vector<int> test;
-  vector<int> result(9);
-  test.push_back(0);
+  vector<int> result(8);
   
-  for (int i = 0; i < 9; ++i)
+  for (int i = 0; i < 8; ++i)
 	{
-	  test.push_back(1);
+	  test.push_back(i);
 	  cout<<test[i]<<" ";
 	}
   cout<<endl;
   prefix_sum(test, result);
   
-  for (int i = 1; i < 9; ++i)
+  for (int i = 0; i < 8; ++i)
 	{
 	  cout<<result[i]<<" ";
 	}
